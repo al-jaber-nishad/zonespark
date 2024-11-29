933c445d7e8f68e23b1512e62a251b2a626bdafc
aj_nishad <mailnishad02@gmail.com>
2024-11-29 23:19:05 +0600

    feat: migrate db from sqlite3 to postgresql

--- core/settings.py
+++ core/settings.py
@@ -97,8 +97,11 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "zonespark_db",
+        'USER': "postgres",
+        'PASSWORD': "12345",
+        'HOST': "localhost",
     }
 }
 
